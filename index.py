fa69068fa65178cb5209e892588729579849f36c
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-09-16 18:01:14 +0900

    fix:復習

--- index.py
+++ index.py
@@ -1,11 +1,38 @@
-initial_balance = 10000
-rate = 1.1
-print(initial_balance)
-print(initial_balance * rate)
-print(initial_balance * rate * rate)
+# initial_balance = 10000
+# rate = 1.1
+# print(initial_balance)
+# print(initial_balance * rate)
+# print(initial_balance * rate * rate)
 
-initial_balance += 1000
-rate = 1.1
-print(initial_balance)
-print(initial_balance * rate)
-print(initial_balance * rate * rate)
+# initial_balance += 1000
+# rate = 1.1
+# print(initial_balance)
+# print(initial_balance * rate)
+# print(initial_balance * rate * rate)
+
+
+# fname = "田中"
+# lname = "太郎"
+# age = 25
+
+# print(f"{fname} {lname} は {age} 歳です")
+
+# num = input("番号は？")
+# print(type(num))
+# print(int(num) *2)
+
+# 条件分岐
+# signal = input("信号の色は？")
+
+# match signal:
+#     case "red":
+#         print("止まれ！")
+#     case "green" | "blue":
+#         print("進め！")
+#     case "yellow":
+#         print("気をつけろ！")
+#     case _:
+#         print("どの色にも当てはまりません")
+
+for i in range(3,9,2):
+    print(f"{i}おはよう！")
