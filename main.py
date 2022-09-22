f77901d4162b6a385239a1b65131228997c2196d
YukiUchinomiya <1mm51f13385411f@gmail.com>
2022-09-22 22:20:11 +0900

    add:lamda関数使用

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-def double(number):
-    return number * 2
+# def double(number):
+#     return number * 2
 
 
-def price(a,b):
-    print(a + b)
+# def price(a,b):
+#     print(a + b)
 
-price(300,700)
+# price(300,700)
+
+# def duble(n):
+#     return n * 2
+
+# print(duble(10))
+
+def double(n):
+    return n * 2
+
+def triple(n):
+    return n *3
+
+def calc(n,func):
+    return func(n)
+
+print(calc(20,lambda n: n * 35 ))
+
