86ce21ab1710827ec65bc387831df7a5713bef58
YukiUchinomiya <1mm51f13385411f@gmail.com>
2023-01-14 18:33:12 +0900

    practice add

---
+++ python_programming/work/practice.py
@@ -0,0 +1,30 @@
+# print('hi','yuki', sep=',',end='\n')
+# print('hi','yuki', sep=',',end='\n')
+# print(17/3)
+
+# x = 5
+# y = 10
+
+# print(x * y)
+
+# flo = 3.1457675756
+# a = round(flo,2)
+# print(a)
+
+# print("""
+#     てんか
+#     ゆうき
+#     西山
+#     """)
+
+print('yuki\n' * 3)
+
+word = 'python'
+print(word[0:4])
+
+str = 'hello!!yukiuchinomiya'
+is_start = str.startswith('he')
+print(is_start)
+
+a = ' this is pen'
+print(f'yuki{a}')
